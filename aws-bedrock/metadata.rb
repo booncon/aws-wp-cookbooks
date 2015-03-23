f2d9fe7ad0d044997519b1da32e481ff83ae4a5e
Lukas Jakob Hafner <luki@booncon.com>
2015-03-23 17:20:26 +0200

    fixing wrong link in metadata

--- aws-bedrock/metadata.rb
+++ aws-bedrock/metadata.rb
@@ -3,7 +3,6 @@ maintainer       "booncon"
 maintainer_email "luki@booncon.com"
 license          "MIT"
 description      "Recipe to set up everything for bedrock + composer @ opsworks"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "composer"   
 
