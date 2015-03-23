cd2aafc8063d888bc044986f5f6c676156690d26
Lukas Jakob Hafner <luki@booncon.com>
2015-03-23 20:19:41 +0200

    env fixed protocol

--- wordpress/recipes/configure.rb
+++ wordpress/recipes/configure.rb
@@ -35,7 +35,7 @@ node[:deploy].each do |app_name, deploy|
             :user       => (deploy[:database][:username] rescue nil),
             :password   => (deploy[:database][:password] rescue nil),
             :host       => (deploy[:database][:host] rescue nil),
-            :protocol   => (deploy[:ssl_certificate] !== nil ? 'https://' : 'http://'),
+            :protocol   => (deploy[:ssl_certificate] != nil ? 'https://' : 'http://'),
             :domain     => (deploy[:domains][0] rescue nil),
             :stage     => (deploy[:environment_variables][:stage] rescue nil),
             :keys       => (keys rescue nil)
