b20d8058abe64adb3e28377c979236a9ca4d3a31
Georgios Tsambikakis <joge81@gmail.com>
2016-02-02 11:21:45 +0200

    Added basic nginx setup for wordpress.

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -6,3 +6,6 @@ default['wordpress']['wp_config']['enable_W3TC'] = false
 
 # Force logins via https (http://codex.wordpress.org/Administration_Over_SSL#To_Force_SSL_Logins_and_SSL_Admin_Access)
 default['wordpress']['wp_config']['force_secure_logins'] = false
+
+# WP HTTP_ROOT
+default["phpapp"]["path"] = "/var/www/wp/"
---
+++ wordpress/recipes/setup.rb
@@ -0,0 +1,59 @@
+app = search(:aws_opsworks_app).first
+app_path = "/srv/#{app['shortname']}"
+
+apt_package "nginx-extras" do
+action :install
+end
+
+apt_package "php5-fpm" do
+action :install
+end
+
+apt_package "php5-mysql" do
+action :install
+end
+
+git node["phpapp"]["path"] do
+  repository "#{app['appsource']['url']}"
+  reference "deploy"
+  action :sync
+end
+
+directory node["phpapp"]["path"] do
+  owner "www-data"
+  group "www-data"
+  mode "0755"
+  action :create
+  recursive true
+end
+
+template "/etc/nginx/nginx.conf" do
+ source "nginx.conf.erb"
+ owner "root"
+ group "www-data"
+ mode "640"
+ notifies :run, "execute[reload-nginx]"
+end
+
+template "/etc/nginx/sites-available/nginx-wordpress.conf" do
+ source "nginx-wordpress.conf.erb"
+ owner "root"
+ group "www-data"
+ mode "640"
+ notifies :run, "execute[reload-nginx]"
+end
+
+link "/etc/nginx/sites-enabled/nginx-wordpress.conf" do
+  to "/etc/nginx/sites-available/nginx-wordpress.conf"
+end
+
+file "/etc/nginx/sites-enabled/default" do
+  action :delete
+  only_if "test -f /etc/nginx/sites-enabled/default"
+  notifies :run, "execute[reload-nginx]"
+end
+
+execute "reload-nginx" do
+  command "nginx -t && service nginx reload"
+  action :nothing
+end
