1c2d3f6ef0ddefbc3a2c5204fe19b3ea963e9555
Georgios Tsambikakis <joge81@gmail.com>
2016-02-05 15:45:51 +0200

    Using export instead of writing to php pool config.

--- wordpress/recipes/setenv.rb
+++ wordpress/recipes/setenv.rb
@@ -2,17 +2,17 @@ db = search("aws_opsworks_rds_db_instance").first
 app = search("aws_opsworks_app").first
 
 execute "set-db-name" do
-  command "echo 'env[DB_NAME] = #{app['data_sources']['database_name']}' >> /etc/php5/fpm/pool.d/www.conf"
+  command "export DB_NAME = #{app['data_sources']['database_name']}"
 end
 
 execute "set-db-host" do
-  command "echo 'env[DB_HOST] = #{db['address']}' >> /etc/php5/fpm/pool.d/www.conf"
+  command "export DB_HOST = #{db['address']}"
 end
 
 execute "set-db-user" do
-  command "echo 'env[DB_USER] = #{db['db_user']}' >> /etc/php5/fpm/pool.d/www.conf"
+  command "export DB_USER = #{db['db_user']}"
 end
 
 execute "set-db-password" do
-  command "echo 'env[DB_PASSWORD] = #{db['db_password']}' >> /etc/php5/fpm/pool.d/www.conf"
+  command "export DB_PASSWORD = #{db['db_password']}"
 end
