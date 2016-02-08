dd8af1442ef9ef814d48c39eabfc7871ed3e2dde
Georgios Tsambikakis <joge81@gmail.com>
2016-02-08 12:57:37 +0200

    Syntax fix.

--- wordpress/recipes/createenvfile.rb
+++ wordpress/recipes/createenvfile.rb
@@ -28,45 +28,45 @@ execute "set-db-password" do
 end
 
 execute "set-wp-env" do
-  "command echo WP_ENV=#{app['environment']['WP_ENV']} >> #{envfile}"
+  command "echo WP_ENV=#{app['environment']['WP_ENV']} >> #{envfile}"
 end
 
 execute "set-wp-home" do
-  "command echo WP_HOME=#{app['environment']['WP_HOME']} >> #{envfile}"
+  command "echo WP_HOME=#{app['environment']['WP_HOME']} >> #{envfile}"
 end
 
 execute "set-wp-siteurl" do
-  "command echo WP_SITEURL=#{app['environment']['WP_SITEURL']} >> #{envfile}"
+  command "echo WP_SITEURL=#{app['environment']['WP_SITEURL']} >> #{envfile}"
 end
 
 execute "set-auth-key" do
-  "command echo AUTH_KEY='#{app['environment']['AUTH_KEY']}' >> #{envfile}"
+  command "echo AUTH_KEY='#{app['environment']['AUTH_KEY']}' >> #{envfile}"
 end
 
 execute "set-secure-auth-key" do
-  "command echo SECURE_AUTH_KEY='#{app['environment']['SECURE_AUTH_KEY']}' >> #{envfile}"
+  command "echo SECURE_AUTH_KEY='#{app['environment']['SECURE_AUTH_KEY']}' >> #{envfile}"
 end
 
 execute "set-logged-in-key" do
-  "command echo LOGGED_IN_KEY='#{app['environment']['LOGGED_IN_KEY']}' >> #{envfile}"
+  command "echo LOGGED_IN_KEY='#{app['environment']['LOGGED_IN_KEY']}' >> #{envfile}"
 end
 
 execute "set-nonce-key" do
-  "command echo NONCE_KEY='#{app['environment']['NONCE_KEY']}' >> #{envfile}"
+  command "echo NONCE_KEY='#{app['environment']['NONCE_KEY']}' >> #{envfile}"
 end
 
 execute "set-auth-salt" do
-  "command echo AUTH_SALT='#{app['environment']['AUTH_SALT']}' >> #{envfile}"
+  command "echo AUTH_SALT='#{app['environment']['AUTH_SALT']}' >> #{envfile}"
 end
 
 execute "set-secure-auth-salt" do
-  "command echo SECURE_AUTH_SALT='#{app['environment']['SECURE_AUTH_SALT']}' >> #{envfile}"
+  command "echo SECURE_AUTH_SALT='#{app['environment']['SECURE_AUTH_SALT']}' >> #{envfile}"
 end
 
 execute "set-logged-in-salt" do
-  "command echo LOGGED_IN_SALT='#{app['environment']['LOGGED_IN_SALT']}' >> #{envfile}"
+  command "echo LOGGED_IN_SALT='#{app['environment']['LOGGED_IN_SALT']}' >> #{envfile}"
 end
 
 execute "set-nonce-salt" do
-  "command echo NONCE_SALT='#{app['environment']['NONCE_SALT']}' >> #{envfile}"
+  command "echo NONCE_SALT='#{app['environment']['NONCE_SALT']}' >> #{envfile}"
 end
--- wordpress/recipes/setenv.rb
+++ wordpress/recipes/setenv.rb
@@ -19,45 +19,45 @@ execute "set-db-password" do
 end
 
 execute "set-wp-env" do
-  "command echo WP_ENV=#{app['environment']['WP_ENV']} >> /etc/environment"
+  command "echo WP_ENV=#{app['environment']['WP_ENV']} >> /etc/environment"
 end
 
 execute "set-wp-home" do
-  "command echo WP_HOME=#{app['environment']['WP_HOME']} >> /etc/environment"
+  command "echo WP_HOME=#{app['environment']['WP_HOME']} >> /etc/environment"
 end
 
 execute "set-wp-siteurl" do
-  "command echo WP_SITEURL=#{app['environment']['WP_SITEURL']} >> /etc/environment"
+  command "echo WP_SITEURL=#{app['environment']['WP_SITEURL']} >> /etc/environment"
 end
 
 execute "set-auth-key" do
-  "command echo AUTH_KEY='#{app['environment']['AUTH_KEY']}' >> /etc/environment"
+  command "echo AUTH_KEY='#{app['environment']['AUTH_KEY']}' >> /etc/environment"
 end
 
 execute "set-secure-auth-key" do
-  "command echo SECURE_AUTH_KEY='#{app['environment']['SECURE_AUTH_KEY']}' >> /etc/environment"
+  command "echo SECURE_AUTH_KEY='#{app['environment']['SECURE_AUTH_KEY']}' >> /etc/environment"
 end
 
 execute "set-logged-in-key" do
-  "command echo LOGGED_IN_KEY='#{app['environment']['LOGGED_IN_KEY']}' >> /etc/environment"
+  command "echo LOGGED_IN_KEY='#{app['environment']['LOGGED_IN_KEY']}' >> /etc/environment"
 end
 
 execute "set-nonce-key" do
-  "command echo NONCE_KEY='#{app['environment']['NONCE_KEY']}' >> /etc/environment"
+  command "echo NONCE_KEY='#{app['environment']['NONCE_KEY']}' >> /etc/environment"
 end
 
 execute "set-auth-salt" do
-  "command echo AUTH_SALT='#{app['environment']['AUTH_SALT']}' >> /etc/environment"
+  command "echo AUTH_SALT='#{app['environment']['AUTH_SALT']}' >> /etc/environment"
 end
 
 execute "set-secure-auth-salt" do
-  "command echo SECURE_AUTH_SALT='#{app['environment']['SECURE_AUTH_SALT']}' >> /etc/environment"
+  command "echo SECURE_AUTH_SALT='#{app['environment']['SECURE_AUTH_SALT']}' >> /etc/environment"
 end
 
 execute "set-logged-in-salt" do
-  "command echo LOGGED_IN_SALT='#{app['environment']['LOGGED_IN_SALT']}' >> /etc/environment"
+  command "echo LOGGED_IN_SALT='#{app['environment']['LOGGED_IN_SALT']}' >> /etc/environment"
 end
 
 execute "set-nonce-salt" do
-  "command echo NONCE_SALT='#{app['environment']['NONCE_SALT']}' >> /etc/environment"
+  command "echo NONCE_SALT='#{app['environment']['NONCE_SALT']}' >> /etc/environment"
 end
