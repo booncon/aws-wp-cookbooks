d2cc006e188e37aa42d87dc457750a25d0567288
Georgios Tsambikakis <joge81@gmail.com>
2016-02-08 12:45:02 +0200

    Added variables.

--- wordpress/recipes/setenv.rb
+++ wordpress/recipes/setenv.rb
@@ -17,3 +17,47 @@ end
 execute "set-db-password" do
   command "echo DB_PASSWORD=#{db['db_password']} >> /etc/environment"
 end
+
+execute "set-wp-env" do
+  "command echo WP_ENV=#{app['environment']['WP_ENV']} >> /etc/environment"
+end
+
+execute "set-wp-home" do
+  "command echo WP_HOME=#{app['environment']['WP_HOME']} >> /etc/environment"
+end
+
+execute "set-wp-siteurl" do
+  "command echo WP_SITEURL=#{app['environment']['WP_SITEURL']} >> /etc/environment"
+end
+
+execute "set-auth-key" do
+  "command echo AUTH_KEY='#{app['environment']['AUTH_KEY']}' >> /etc/environment"
+end
+
+execute "set-secure-auth-key" do
+  "command echo SECURE_AUTH_KEY='#{app['environment']['SECURE_AUTH_KEY']}' >> /etc/environment"
+end
+
+execute "set-logged-in-key" do
+  "command echo LOGGED_IN_KEY='#{app['environment']['LOGGED_IN_KEY']}' >> /etc/environment"
+end
+
+execute "set-nonce-key" do
+  "command echo NONCE_KEY='#{app['environment']['NONCE_KEY']}' >> /etc/environment"
+end
+
+execute "set-auth-salt" do
+  "command echo AUTH_SALT='#{app['environment']['AUTH_SALT']}' >> /etc/environment"
+end
+
+execute "set-secure-auth-salt" do
+  "command echo SECURE_AUTH_SALT='#{app['environment']['SECURE_AUTH_SALT']}' >> /etc/environment"
+end
+
+execute "set-logged-in-salt" do
+  "command echo LOGGED_IN_SALT='#{app['environment']['LOGGED_IN_SALT']}' >> /etc/environment"
+end
+
+execute "set-nonce-salt" do
+  "command echo NONCE_SALT='#{app['environment']['NONCE_SALT']}' >> /etc/environment"
+end
