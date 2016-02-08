271143e162648fc3ad2cccfb2a739e550038e78e
Georgios Tsambikakis <joge81@gmail.com>
2016-02-08 15:03:26 +0200

    Using a template instead of writing directly to a file.

--- wordpress/recipes/createenvfile.rb
+++ wordpress/recipes/createenvfile.rb
@@ -1,72 +1,31 @@
 db = search("aws_opsworks_rds_db_instance").first
 app = search("aws_opsworks_app").first
+app_db = app['data_sources'].first
 
 envfile = "#{node['phpapp']['path']}.env"
 
-file "#{envfile}" do
-  owner "www-data"
-  group "www-data"
+template "#{envfile}" do
+  source "env.erb"
   mode "0644"
+  group "www-data"
+  owner "www-data"
   action [:delete, :create]
-end
 
-execute "set-db-name" do
-  app_db = app['data_sources'].first
-  command "echo DB_NAME=#{app_db['database_name']} >> #{envfile}"
-end
-
-execute "set-db-host" do
-  command "echo DB_HOST=#{db['address']} >> #{envfile}"
-end
-
-execute "set-db-user" do
-  command "echo DB_USER=#{db['db_user']} >> #{envfile}"
-end
-
-execute "set-db-password" do
-  command "echo DB_PASSWORD=#{db['db_password']} >> #{envfile}"
-end
-
-execute "set-wp-env" do
-  command "echo WP_ENV=#{app['environment']['WP_ENV']} >> #{envfile}"
-end
-
-execute "set-wp-home" do
-  command "echo WP_HOME=#{app['environment']['WP_HOME']} >> #{envfile}"
-end
-
-execute "set-wp-siteurl" do
-  command "echo WP_SITEURL=#{app['environment']['WP_SITEURL']} >> #{envfile}"
-end
-
-execute "set-auth-key" do
-  command "echo AUTH_KEY='#{app['environment']['AUTH_KEY']}' >> #{envfile}"
-end
-
-execute "set-secure-auth-key" do
-  command "echo SECURE_AUTH_KEY='#{app['environment']['SECURE_AUTH_KEY']}' >> #{envfile}"
-end
-
-execute "set-logged-in-key" do
-  command "echo LOGGED_IN_KEY='#{app['environment']['LOGGED_IN_KEY']}' >> #{envfile}"
-end
-
-execute "set-nonce-key" do
-  command "echo NONCE_KEY='#{app['environment']['NONCE_KEY']}' >> #{envfile}"
-end
-
-execute "set-auth-salt" do
-  command "echo AUTH_SALT='#{app['environment']['AUTH_SALT']}' >> #{envfile}"
-end
-
-execute "set-secure-auth-salt" do
-  command "echo SECURE_AUTH_SALT='#{app['environment']['SECURE_AUTH_SALT']}' >> #{envfile}"
-end
-
-execute "set-logged-in-salt" do
-  command "echo LOGGED_IN_SALT='#{app['environment']['LOGGED_IN_SALT']}' >> #{envfile}"
-end
-
-execute "set-nonce-salt" do
-  command "echo NONCE_SALT='#{app['environment']['NONCE_SALT']}' >> #{envfile}"
+  variables(
+    :db_name          =>  "#{app_db['database_name']}",
+    :db_host          =>  "#{db['address']}",
+    :db_user          =>  "#{db['db_user']}",
+    :db_password      =>  "#{db['db_password']}",
+    :wp_env           =>  "#{app['environment']['WP_ENV']}",
+    :wp_home          =>  "#{app['environment']['WP_HOME']}",
+    :wp_siteurl       =>  "#{app['environment']['WP_SITEURL']}",
+    :auth_key         =>  "#{app['environment']['AUTH_KEY']}",
+    :secure_auth_key  =>  "#{app['environment']['SECURE_AUTH_KEY']}",
+    :logged_in_key    =>  "#{app['environment']['LOGGED_IN_KEY']}",
+    :nonce_key        =>  "#{app['environment']['NONCE_KEY']}",
+    :auth_salt        =>  "#{app['environment']['AUTH_SALT']}",
+    :secure_auth_salt =>  "#{app['environment']['SECURE_AUTH_SALT']}",
+    :logged_in_salt   =>  "#{app['environment']['LOGGED_IN_SALT']}",
+    :nonce_salt       =>  "#{app['environment']['NONCE_SALT']}"
+  )
 end
