da3240c97de702fa4cbe40133e2af315729d9d4f
Georgios Tsambikakis <joge81@gmail.com>
2016-02-09 16:56:35 +0200

    Deploy script.

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -1,19 +1,58 @@
-# Create the Wordpress config file wp-config.php with corresponding values
-node[:deploy].each do |app_name, deploy|
+app = search("aws_opsworks_app").first
+user = search("aws_opsworks_user").first
 
-    template "#{deploy[:deploy_to]}/shared/.env" do
-        source "env.erb"
-        mode 0660
-        group deploy[:group]
+site_root = "#{node['web_root']}#{app['environment']['THEME_NAME']}/"
+current_link = "#{site_root}current"
+time =  Time.new.strftime("%Y%m%d%H%M%S")
+release_dir = "#{site_root}releases/#{time}/"
+shared_upload_dir = "#{site_root}shared/web/app/uploads/"
 
-        owner "apache"
-        
-        variables(
-            :database   => (deploy[:database][:database] rescue nil),
-            :user       => (deploy[:database][:username] rescue nil),
-            :password   => (deploy[:database][:password] rescue nil),
-            :host       => (deploy[:database][:host] rescue nil),
-            :keys       => (keys rescue nil)
-        )
-    end	
+count_command = "ls -l #{site_root}releases/ | grep ^d | wc -l"
+directory_count = shell_out(count_command)
+
+if directory_count.stdout.to_i > 4
+  execute "delete-oldest-release" do
+    command "find #{site_root}releases/* -maxdepth 0 -type d -print | sort | head -n 1 | xargs rm -rf"
+  end
+end
+
+directory "#{release_dir}" do
+  owner "www-data"
+  group "www-data"
+  mode "2775"
+  action :create
+  recursive true
+end
+
+execute "ssh-git-clone" do
+  command "ssh-agent sh -c 'ssh-add /home/#{user['username']}/.ssh/id_rsa; git clone #{app['app_source']['url']} #{release_dir}'"
+end
+
+directory "#{release_dir}web/app/uploads" do
+  recursive true
+  action :delete
+end
+
+link "#{current_link}" do
+  action :delete
+end
+
+link "#{current_link}" do
+  to "#{release_dir}"
+end
+
+link "#{release_dir}web/app/uploads" do
+  to "#{shared_upload_dir}"
+end
+
+execute "change-directory-permissions" do
+  command "find #{release_dir} -type d -exec chmod 2775 {} +"
+end
+
+execute "change-file-permissions" do
+  command "find #{release_dir} -type f -exec chmod 0664 {} +"
+end
+
+execute "change-ownership" do
+  command "chown -R www-data:www-data #{release_dir}"
 end
