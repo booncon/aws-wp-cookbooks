7cde0aa97a19cb31c113824004f0762b27b183ae
Lukas Jakob Hafner <luki@booncon.com>
2016-07-16 12:43:42 +0300

    fixed apt-get install in efs mount recipe

--- efs/recipes/setup.rb
+++ efs/recipes/setup.rb
@@ -2,17 +2,17 @@ efs_root = "/efs"
 
 if !Dir.exists?("#{efs_root}")
 
-  execute "install-nfs-common" do
-    command "apt-get install nfs-common"
+  apt_package "nfs-common" do
+    action :install
+  end
+
+  execute "mount-efs" do
+    command "sudo mount -t nfs4 -o nfsvers=4.1 $(curl -s #{node['efs_mount']}:/ efs"
   end
 
   directory "#{efs_root}" do
     action :create
     recursive true
-  end
-
-  execute "mount-efs" do
-    command "sudo mount -t nfs4 -o nfsvers=4.1 $(curl -s #{node['efs_mount']}:/ efs"
   end
 
 end
