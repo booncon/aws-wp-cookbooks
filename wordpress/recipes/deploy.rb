89de3d67dc89e26a22d2cb7e485603e887ac3774
Lukas Jakob Hafner <luki@booncon.com>
2017-10-24 12:18:23 +0200

    fix php-timeout

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -125,7 +125,7 @@ search("aws_opsworks_app").each do |app|
         :domains => domains,
         :app_name => app['shortname'],
         :enable_ssl => app['enable_ssl'],
-        :php_timeout => app['php_timeout']
+        :php_timeout => node['php_timeout']
       )
     end
 
