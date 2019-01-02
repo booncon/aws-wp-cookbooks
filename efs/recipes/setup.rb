fd9fceccf0db09c6973b411c8e0114a3f7225d81
Lukas Jakob Hafner <luki@booncon.com>
2019-01-02 10:51:23 +0100

    updated efs mount to new syntax

--- efs/recipes/setup.rb
+++ efs/recipes/setup.rb
@@ -1,16 +1,18 @@
 efs_root = "/efs"
 
 if !Dir.exists?("#{efs_root}")
+
+  apt_package "nfs-common" do
+    action :install
+  end
+
   directory "#{efs_root}" do
     action :create
     recursive true
   end
-end
 
-apt_package "nfs-common" do
-  action :install
-end
+  execute "mount-efs" do
+    command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport #{node['efs_mount']}:/ /efs"
+  end
 
-execute "mount-efs" do
-  command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s #{node['efs_mount']}:/ /efs"
 end
