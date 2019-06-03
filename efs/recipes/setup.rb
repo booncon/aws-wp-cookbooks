432a7ed18e18430a0b7a70cf3c66387bde222998
Markus Ilmer <markus.ilmer@gmail.com>
2019-06-03 22:46:56 +0200

    always mount efs

--- efs/recipes/setup.rb
+++ efs/recipes/setup.rb
@@ -2,17 +2,17 @@ efs_root = "/efs"
 
 if !Dir.exists?("#{efs_root}")
 
-  apt_package "nfs-common" do
-    action :install
-  end
-
   directory "#{efs_root}" do
     action :create
     recursive true
   end
 
-  execute "mount-efs" do
-    command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport #{node['efs_mount']}:/ /efs"
-  end
+end
 
+apt_package "nfs-common" do
+  action :install
+end
+
+execute "mount-efs" do
+  command "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport #{node['efs_mount']}:/ /efs"
 end
